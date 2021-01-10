7dbdbd80b7dbd3a59053766881f092f9379268d2
satoshi-harakudashi <74819955+satoshi-harakudashi@users.noreply.github.com>
2021-01-10 18:50:29 +0900

    作成3

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour
+{
+    private GameObject unityChan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unityChan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z < unityChan.transform.position.z-5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
